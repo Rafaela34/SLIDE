6d6925c91fdac84f90733805360823acba56e585
RAFAELA SANTOS DA SILVA <21193422@CFP127ALUNO.LOCAL>
2021-10-04 16:55:34 -0300

    criei o html e css

---
+++ cards/card.js
@@ -0,0 +1,9 @@
+"use strict"
+
+const herois = [
+    'Irond - Wounded, captured and forced to build a weapon by …make the world a safer, better place as Iron Man.',
+    'Thanos - The Mad Titan Thanos, a melancholy, brooding indiv…he became more powerful than any of his brethren.',
+    'SpiderMan - Peter’s relatively normal life changed abruptly du…it escaped and bit Peter before it was destroyed.',
+    'Thor - As the Norse God of thunder and lightning, Thor wi…never, ever stop fighting for a worthwhile cause.',
+    'CaptainAmerica - Vowing to serve his country any way he could, youn… living, breathing symbol of freedom and liberty.'
+]
